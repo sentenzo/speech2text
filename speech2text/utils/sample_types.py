66b16ca457dddfaaaca7b3938cc2c20875db12c8
sentenzo <sentenzo@users.noreply.github.com>
2024-01-24 12:03:00 +0300

    working with audio-samples

---
+++ speech2text/utils/sample_types.py
@@ -0,0 +1,139 @@
+from enum import Enum, auto
+from typing import Any
+
+import numpy as np
+
+MAX_INT32 = 1 << 31
+
+
+class SampleDType(Enum):
+    BYTES_2 = auto()
+    BYTES_4 = auto()
+    NP_I16 = np.int16
+    NP_I32 = np.int32
+    NP_F16 = np.float16
+    NP_F32 = np.float32
+
+    @staticmethod
+    def guess(data: Any):
+        if not data:
+            raise ValueError
+        if isinstance(data, bytes | bytearray):
+            return SampleDType.BYTES_2
+        if not isinstance(data, np.ndarray | list):
+            raise ValueError
+        val = data[0]
+        if isinstance(val, np.int16 | int):
+            return SampleDType.NP_I16
+        if isinstance(val, np.int32):
+            return SampleDType.NP_I32
+        if isinstance(val, np.float16):
+            return SampleDType.NP_F16
+        if isinstance(val, np.float32 | float):
+            return SampleDType.NP_F32
+        raise ValueError
+
+
+def _b2_to_i32(data: bytes | bytearray) -> np.int32:
+    return np.frombuffer(data, "<i2").astype(np.int32)  # little endian
+
+
+def _b4_to_i32(data: bytes | bytearray) -> np.int32:
+    return np.frombuffer(data, "<i4")
+
+
+def _i16_to_i32(data: np.int16) -> np.int32:
+    return np.array(data).astype(np.int32)
+
+
+def _i32_to_i32(data: np.int32 | int) -> np.int32:
+    # return data
+    return np.array(data)
+
+
+def _f16_to_i32(data: np.float16) -> np.int32:
+    data = np.array(data).astype(np.float32)
+    data *= MAX_INT32
+    return data.astype(np.int32)
+
+
+def _f32_to_i32(data: np.float32 | float) -> np.int32:
+    data = np.array(data) * MAX_INT32
+    return data.astype(np.int32)
+
+
+CONVERT_TO_INT32 = {
+    SampleDType.BYTES_2: _b2_to_i32,
+    SampleDType.BYTES_4: _b4_to_i32,
+    SampleDType.NP_I16: _i16_to_i32,
+    SampleDType.NP_I32: _i32_to_i32,
+    SampleDType.NP_F16: _f16_to_i32,
+    SampleDType.NP_F32: _f32_to_i32,
+}
+
+
+def _i32_to_b2(data: np.int32 | int) -> bytes:
+    data = np.array(data, dtype="<i4")  # little endian int32
+    return data.tobytes()
+
+
+def _i32_to_i16(data: np.int32 | int) -> np.int16:
+    data = np.array(data, dtype="<i4")
+    data >>= 16
+    return data.astype("<i2")
+
+
+def _i32_to_b4(data: np.int32 | int) -> bytes:
+    return _i32_to_i16(data).tobytes()
+
+
+def _i32_to_f16(data: np.int32 | int) -> np.float16:
+    data = np.array(data, dtype="<i4")
+    return np.array(data / MAX_INT32, dtype="f2")
+
+
+def _i32_to_f32(data: np.int32 | int) -> np.float32:
+    data = np.array(data, dtype="<i4")
+    return np.array(data / MAX_INT32, dtype="f4")
+
+
+CONVERT_FROM_INT32 = {
+    SampleDType.BYTES_2: _i32_to_b2,
+    SampleDType.BYTES_4: _i32_to_b4,
+    SampleDType.NP_I16: _i32_to_i16,
+    SampleDType.NP_I32: _i32_to_i32,
+    SampleDType.NP_F16: _i32_to_f16,
+    SampleDType.NP_F32: _i32_to_f32,
+}
+
+CONVERT_FROM_TO = {}
+for frm in SampleDType:
+    CONVERT_FROM = CONVERT_FROM_TO.get(frm, {})
+    for to in SampleDType:
+
+        def convert(data):
+            data = CONVERT_TO_INT32[frm](data)
+            return CONVERT_FROM_INT32[to](data)
+
+        CONVERT_FROM[to] = convert
+
+TYPE_FACTORY = {
+    SampleDType.BYTES_2: bytearray,
+    SampleDType.BYTES_4: bytearray,
+    SampleDType.NP_I16: lambda arr: np.array(arr, "<i2"),
+    SampleDType.NP_I32: lambda arr: np.array(arr, "<i4"),
+    SampleDType.NP_F16: lambda arr: np.array(arr, "f2"),
+    SampleDType.NP_F32: lambda arr: np.array(arr, "f4"),
+}
+
+BYTES_EXTEND = lambda arr, add: arr.extend(add)
+NP_EXTEND = (lambda arr, add: np.append(arr, add),)
+
+TYPE_EXTEND_FUNC = {
+    SampleDType.BYTES_2: BYTES_EXTEND,
+    SampleDType.BYTES_4: BYTES_EXTEND,
+    SampleDType.NP_I16: NP_EXTEND,
+    SampleDType.NP_I32: NP_EXTEND,
+    SampleDType.NP_F16: NP_EXTEND,
+    SampleDType.NP_F32: NP_EXTEND,
+}
--- speech2text/utils/samples.py
+++ speech2text/utils/samples.py
@@ -1,114 +1,68 @@
 from __future__ import annotations
 
-from enum import Enum, auto
 from typing import Any
 
-import numpy as np
-
-MAX_INT32 = 1 << 31
-
-
-class SampleDataType(Enum):
-    BYTES_2 = auto()
-    BYTES_4 = auto()
-    NP_I16 = np.int16
-    NP_I32 = np.int32
-    NP_F16 = np.float16
-    NP_F32 = np.float32
-
-    @staticmethod
-    def guess(data: Any):
-        if not data:
-            raise ValueError
-        if isinstance(data, bytes | bytearray):
-            return SampleDataType.BYTES
-        if not isinstance(data, np.ndarray | list):
-            raise ValueError
-        val = data[0]
-        if isinstance(val, np.int16 | int):
-            return SampleDataType.NP_I16
-        if isinstance(val, np.int32):
-            return SampleDataType.NP_I32
-        if isinstance(val, np.float16):
-            return SampleDataType.NP_F16
-        if isinstance(val, np.float32 | float):
-            return SampleDataType.NP_F32
+from speech2text.utils.sample_types import (
+    CONVERT_FROM_INT32,
+    CONVERT_FROM_TO,
+    CONVERT_TO_INT32,
+    TYPE_EXTEND_FUNC,
+    TYPE_FACTORY,
+)
+from speech2text.utils.sample_types import SampleDType as sdt
 
 
-def _b2_to_i32(data: bytes | bytearray) -> np.int32:
-    np_datatype = np.dtype(np.int16)
-    np_datatype = np_datatype.newbyteorder("L")  # little endian
-    data = np.frombuffer(data, np_datatype)
-    return data.astype(np.int32)
+class Samples:
+    _main_types = (
+        sdt.BYTES_2,
+        sdt.NP_I32,
+        sdt.NP_F32,
+    )
 
+    def __init__(self, init_data: Any = None, init_dtype: sdt = None) -> None:
+        if isinstance(init_data, Samples):
+            self._sample_data = init_data._sample_data
+            return
 
-def _b4_to_i32(data: bytes | bytearray) -> np.int32:
-    np_datatype = np.dtype(np.int32)
-    np_datatype = np_datatype.newbyteorder("L")  # little endian
-    data = np.frombuffer(data, np_datatype)
-    return data
+        self._sample_data = {}
+        for st in self._main_types:
+            self._sample_data[st] = TYPE_FACTORY[st]()
+        if not init_data:
+            return
+        if not init_dtype:
+            init_dtype = sdt.guess(init_data)
+        self.update(init_data, init_dtype)
 
+    def as_type(self, dtype: sdt):
+        if dtype not in self._sample_data:
+            i32_data = self._sample_data[sdt.NP_I32]
+            dt_data = CONVERT_FROM_INT32[dtype](i32_data)
+            self._sample_data[dtype] = dt_data
+        return self._sample_data[dtype]
 
-def _i16_to_i32(data: np.int16) -> np.int32:
-    return np.array(data).astype(np.int32)
+    def _drop_non_main_types(self):
+        for st in self._sample_data:
+            if st not in self._main_types:
+                del self._sample_data[st]
 
+    def update(self, data: Any, dtype: sdt):
+        self._sample_data[dtype] = data
+        i32_data = CONVERT_TO_INT32[dtype](data)
+        for st in self._sample_data:
+            if st != dtype:
+                st_data = CONVERT_FROM_INT32[st](i32_data)
+                self._sample_data[st] = st_data
 
-def _i32_to_i32(data: np.int32 | int) -> np.int32:
-    # return data
-    return np.array(data)
+    def extend(self, data: Samples | Any, dtype: sdt = None):
+        if not dtype:
+            if not isinstance(data, Samples):
+                raise ValueError
+            dtype = sdt.NP_I32
+            data = data._sample_data[dtype]
 
-
-def _f16_to_i32(data: np.float16) -> np.int32:
-    data = np.array(data).astype(np.float32)
-    data *= MAX_INT32
-    return data.astype(np.int32)
-
-
-def _f32_to_i32(data: np.float32 | float) -> np.int32:
-    data = np.array(data) * MAX_INT32
-    return data.astype(np.int32)
-
-
-CONVERT_TO_INT32 = {
-    SampleDataType.BYTES_2: _b2_to_i32,
-    SampleDataType.BYTES_4: _b4_to_i32,
-    SampleDataType.NP_I16: _i16_to_i32,
-    SampleDataType.NP_I32: _i32_to_i32,
-    SampleDataType.NP_F16: _f16_to_i32,
-    SampleDataType.NP_F32: _f32_to_i32,
-}
-
-
-def _i32_to_b2(data: np.int32 | int) -> bytes:
-    data = np.array(data, dtype="<i4")  # little endian int32
-    return data.tobytes()
-
-
-def _i32_to_i16(data: np.int32 | int) -> np.int16:
-    data = np.array(data, dtype="<i4")
-    data >>= 16
-    return data.astype("<i2")
-
-
-def _i32_to_b4(data: np.int32 | int) -> bytes:
-    return _i32_to_i16(data).tobytes()
-
-
-def _i32_to_f16(data: np.int32 | int) -> np.float16:
-    data = np.array(data, dtype="<i4")
-    return np.array(data / MAX_INT32, dtype="f2")
-
-
-def _i32_to_f32(data: np.int32 | int) -> np.float32:
-    data = np.array(data, dtype="<i4")
-    return np.array(data / MAX_INT32, dtype="f4")
-
-
-CONVERT_FROM_INT32 = {
-    SampleDataType.BYTES_2: _i32_to_b2,
-    SampleDataType.BYTES_4: _i32_to_b4,
-    SampleDataType.NP_I16: _i32_to_i16,
-    SampleDataType.NP_I32: _i32_to_i32,
-    SampleDataType.NP_F16: _i32_to_f16,
-    SampleDataType.NP_F32: _i32_to_f32,
-}
+        TYPE_EXTEND_FUNC[dtype](self._sample_data[dtype], data)
+        i32_data = CONVERT_TO_INT32[dtype](data)
+        for st in self._sample_data:
+            if st != dtype:
+                st_data = CONVERT_FROM_INT32[st](i32_data)
+                TYPE_EXTEND_FUNC[st](self._sample_data[st], st_data)
