4d667b6712555566547a12d976b97c52ac950746
sentenzo <sentenzo@users.noreply.github.com>
2024-01-27 11:06:45 +0300

    `whisper`-based transformations

--- speech2text/transcriber/transformations/transcribe.py
+++ speech2text/transcriber/transformations/transcribe.py
@@ -7,24 +7,29 @@ from . import Transformation
 
 class Transcribe(Transformation):
     def __init__(self):
-        ...
+        self.model = whisper.load_model("tiny.en", in_memory=True)
 
     def change_state(self, state: State):
+        initial_prompt = None
+        if state.text_finalized:
+            initial_prompt = state.text_finalized[-1]
         for block in state.blocks:
-            np_array = block.data.as_np_float32()
-            ...
+            np_array = block.samples.as_np_float32()
+            transcription = whisper.transcribe(
+                self.model,
+                np_array,
+                fp16=False,
+                condition_on_previous_text=False,
+                initial_prompt=initial_prompt,
+            )
+            block.text = transcription["text"]
+            initial_prompt = block.text
 
 
-# class WhisperTranscriber(Transcriber):
-#     def __init__(self):
-#         super().__init__()
-#         self.model = whisper.load_model("tiny.en", in_memory=True)
-
-#     def transcribe(self, samples: Samples) -> str:
-#         tr = whisper.transcribe(
-#             self.model,
-#             samples.as_type(Sdt.NP_F32),
-#             fp16=False,
-#             condition_on_previous_text=False,
-#         )
-#         return tr["text"]
+class Finalize(Transformation):
+    def change_state(self, state: State):
+        if len(state.blocks) > 1:
+            last_block = state.blocks.pop()
+            for block in state.blocks:
+                state.text_finalized.append(block.text)
+            state.blocks = [last_block]
